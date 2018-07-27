3e014421d160f8977c941b3e5199ba19c1fc83ac
Chanchal Raj <rajchanchalkhatri@gmail.com>
2018-07-27 05:53:11 +0400

    Initial Commit

---
+++ PoolMe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PoolMe
+//
+//  Created by Chanchal Raj on 27/07/2018.
+//  Copyright © 2018 Chanchal Raj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
